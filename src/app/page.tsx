eee8c94532ac610006de162303fe037fa51b1b20
Sijanshakya <shakyasijan01@gmail.com>
2023-10-30 10:59:55 +0545

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Card } from "@/components/card/Card";
 import { Product } from "../../types";
-// import useProduct from '../../useProduct';
 import ProductCard from "@/components/ProductCard/ProductCard";
 import img1 from "/images/img1.jpg";
 
@@ -27,7 +26,7 @@ export default function Home() {
         <ProductCard
           key={product.id}
           product={product}
-          image={<ProductCard.Image src={product.image} />} // Assuming Image component accepts src prop
+          image={<ProductCard.Image src={product.image} />} 
           title={<ProductCard.Title>{product.title}</ProductCard.Title>}
           price={<ProductCard.Price>Rs {product.price}</ProductCard.Price>}
           action={<ProductCard.Button>Add to cart</ProductCard.Button>}
